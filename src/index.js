a28fec17207630e4f7f94b22b2e440113bc4b14b
Harsh Agarwal <agarwalharsh2310@gmail.com>
2025-06-11 20:09:33 +0800

    [Commit 2] Second Commit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import express from 'express';
 import { StatusCodes } from 'http-status-codes';
+
+import connectDB from './config/dbconfig.js';
 import { PORT } from './config/serverConfig.js';
 
-
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.get('/ping', (req, res) => {
     return res.status(StatusCodes.OK).json({ message: 'pong' });
 });
 
 app.listen(PORT, () => {
-    console.log('Server is running on port. ${PORT}');
+    console.log(`Server is running on port. ${PORT}`);
+    connectDB();
 });
 
 // https://github.com/singhsanket143/Messaging-Slack-Backend
